e7d08eeab0af7df4b306d05d4c969b6040fdd98a
Nate Wong <natewong1@gmail.com>
2023-04-18 18:36:11 -0400

    Navigate to project on button click

--- app/components/projects/index.tsx
+++ app/components/projects/index.tsx
@@ -1,12 +1,15 @@
 import type { Database } from "@/lib/database.types"
+import { useNavigate } from "@remix-run/react"
 import { PlusSquare } from "lucide-react"
 import { Button as RAButton } from "react-aria-components"
+
 type Props = {
   projects: Database["public"]["Tables"]["projects"]["Row"][]
   setIsCreateNewProjectModalOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export default function Projects({ projects, setIsCreateNewProjectModalOpen }: Props) {
+  const navigate = useNavigate()
   return (
     <div className="flex mt-4 gap-4 flex-wrap">
       <RAButton
@@ -20,6 +23,7 @@ export default function Projects({ projects, setIsCreateNewProjectModalOpen }: P
       </RAButton>
       {projects?.map((project) => (
         <RAButton
+          onPress={() => navigate(`/projects/${project.id}`)}
           key={project.id}
           className="bg-black cursor-pointer w-80 h-52 rounded-lg border border-zinc-800 data-[hovered]:border-zinc-700 data-[pressed]:border-sky-500 focus:outline-none"
         >
